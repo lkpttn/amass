0d63834849577849dd0a6fdd5d96fca98bdaac92
Luke <lkpttn@gmail.com>
2014-08-13 19:42:24 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,108 @@
+// CONFIG
+// Server and express variables
+var express = require('express');
+var app = express();
+
+// Big objects
+var health = {};
+var location = {};
+var social = {};
+
+this.secrets = require(__dirname + '/secrets/secrets.json');
+var secrets = this.secrets;
+
+// Foursquare client IDs
+var foursquareConfig = {
+  'secrets' : {
+    'clientId' : secrets.foursquare.clientId,
+    'clientSecret' : secrets.foursquare.clientSecret,
+    'redirectUrl' : secrets.foursquare.redirectUrl
+  }
+}
+
+// Foursquare variables
+var Foursquare = require('node-foursquare')(foursquareConfig);
+var foursquareData = {};
+var mostRecentCheckin = null;
+
+// Google Spreadsheet variables
+var GoogleSpreadsheet = require('google-spreadsheet');
+var weightSpreadsheet = new GoogleSpreadsheet(secrets.googleSpreadsheet.weightID);
+var mealsSpreadsheet = new GoogleSpreadsheet(secrets.googleSpreadsheet.mealsID);
+
+
+
+// EXECUTE
+// Gets foursqare checkins
+Foursquare.Users.getCheckins(null, null, secrets.foursquare.accessToken, function (error, data) {
+
+  // Pulls checkins item out of JSON response
+  foursquareData = data.checkins;
+  mostRecentCheckin = data.checkins.items[0];
+
+  //console.log(mostRecentCheckin);
+});
+
+
+
+// Gets weight from google spreadsheet
+weightSpreadsheet.getInfo( function (err, sheet_info) {
+  console.log( sheet_info.title + ' is loaded' );
+  sheet_info.worksheets[0].getRows( function(err, rows) {
+
+    // Variable for the latest measurement
+    var latestDay = rows.length - 1;
+    // // Array to hold the past week
+    var pastWeekWeight = [];
+
+    // Load last 7 days into an array
+    for (i = 0; i < 7; i++) {
+      var pastDay = latestDay - i;
+      pastWeekWeight[i] = rows[pastDay].weight + ' lbs';
+      // console.log(rows[pastDay.date + ' : ' + rows[pastDay].weight + 'lbs');
+    };
+
+    health.weight = rows[latestDay].weight;
+    health.bodyFat = rows[latestDay].fat;
+    health.pastWeekWeight = pastWeekWeight.reverse();
+  });
+});
+
+// Gets meals from google spreadsheet
+mealsSpreadsheet.getInfo( function (err, sheet_info) {
+  console.log( sheet_info.title + ' is loaded' );
+  sheet_info.worksheets[0].getRows( function(err, rows) {
+
+    // Variable for the latest measurement
+    var latestMeal = rows.length - 1;
+
+    health.latestMeal = rows[latestMeal].description;
+    health.latestMealCalories = rows[latestMeal].calories;
+  });
+});
+
+
+
+
+// IDK
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  // Spit out most recent foursquare checkin
+  res.sendfile('index.html');
+});
+
+app.get('/foursquare', function(req, res) {
+  // Spit out most recent foursquare checkin
+  res.json(foursquareData);
+});
+
+app.get('/health', function(req, res) {
+  // Spit out most recent foursquare checkin
+  res.json(health);
+});
+
+// Set up express server on port 3000
+var server = app.listen(3000, function () {
+  console.log('Listening on port %d', server.address().port);
+});
---
+++ public/js/main.js
@@ -0,0 +1,57 @@
+$(function() {
+
+    var updateHealth = function() {
+    $.ajax('/health', {
+      dataType: 'json',
+      type: 'GET',
+      success: function(data) {
+        var healthData = data;
+
+        // Get age - time elapsed
+        var birthday = new Date(1992, 5, 19, 0, 0); // June 19, 1992
+        var now = new Date();
+        var age = now.getTime() - birthday.getTime();
+        age = age/(1000*60*60*24*365);
+
+        // Body
+        $('.age').text(age.toFixed(2) + ' years');
+        $('.weight').text(healthData.weight + 'lbs');
+        $('.bodyFat').text(healthData.bodyFat + '%');
+
+        // Food
+        $('.latestMeal').text(healthData.latestMeal);
+        $('.calories').text(healthData.latestMealCalories);
+      }
+    });
+  };
+
+  updateHealth();
+
+  var updateFoursquare = function() {
+    $.ajax('/foursquare', {
+      dataType: 'json',
+      type: 'GET',
+      success: function(data) {
+        var foursquareData = data;
+        var mostRecentCheckin = data.items[0];
+
+        // Time of most recent checkin
+        var checkinDate = new Date(mostRecentCheckin.createdAt*1000);
+        var hours = checkinDate.getHours();
+        var minutes = checkinDate.getMinutes();
+
+        // Most recent checkin
+        $('.latestCheckin').text(mostRecentCheckin.venue.name).css('color','blue');
+        $('.time').text(hours + ':' + minutes);
+
+        // Loop for last 4 checkins
+        for (i = 1; i < 5; i++) {
+          var tempCheckin = foursquareData.items[i];
+          $('.foursquareRecent'+i).text(tempCheckin.venue.name);
+        }
+      }
+    });
+  };
+
+  updateFoursquare();
+});
